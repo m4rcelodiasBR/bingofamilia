b1a7a94904991df04cf76a22032f3649cd9fa23f
Marcelo Dias <m4rcelodias@live.com>
2025-11-30 22:06:36 -0300

    feat: estrutura inicial do projeto Bingo FullStack

---
+++ src/main/java/br/com/bingofamilia/BingoFamiliaApplication.java
@@ -0,0 +1,13 @@
+package br.com.bingofamilia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BingoFamiliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BingoFamiliaApplication.class, args);
+	}
+
+}
